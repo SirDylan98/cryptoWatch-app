2ae52cee6a900aad549fcec1cb7339cf44118b0c
SirDylan98 <dylandzvenetashinga@gmail.com>
2023-01-23 21:58:35 +0200

    last commit

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   return (
     <div className='rounded-div flex items-center justify-between h-20 font-bold'>
       <Link to='/'>
-        <h1 className='text-2xl'>Cryptobase</h1>
+        <h1 className='text-2xl'>CryptoWatch</h1>
       </Link>
       <div className='hidden md:block'>
         <ThemeToggle />
